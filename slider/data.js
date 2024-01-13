495bf7ecaf168a3d20769159596d25667872e7fa
花间酒 <98141596+flower0wine@users.noreply.github.com>
2024-01-13 22:06:53 +0800

    Update data.js

--- slider/data.js
+++ slider/data.js
@@ -7,13 +7,13 @@
 'use strict';
 
 let data = [{
-    src: "/img/1.jpg",
+    src: "./img/1.jpg",
 }, {
-    src: "/img/2.jpg",
+    src: "./img/2.jpg",
 }, {
-    src: "/img/3.jpg",
+    src: "./img/3.jpg",
 }, {
-    src: "/img/4.jpg",
+    src: "./img/4.jpg",
 }, {
-    src: "/img/5.jpg",
+    src: "./img/5.jpg",
 }]
